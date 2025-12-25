7071043f490c43c03fc92ccec81e51aaf242bc42
Evan Liu <emin@jwpei.com>
2025-12-25 09:27:00 +0000

    监控规则开发

--- models/marts/fct_daily_metrics.sql
+++ models/marts/fct_daily_metrics.sql
@@ -1,23 +1,128 @@
+-------------------------------过去第三十天 成本-------------------------------
 -- 退款
 select 
     `date`,
-    "refund_b_sales_refund_create_usd" as metric_name,
-    sum(refund_b_sales_refund_create_usd) as metric_value,
+    "ALL" as country,
+    "refund_b_total_create_usd" as metric_name,
+    sum(refund_b_total_create_usd) as metric_value,
+    sum(net_profit_total_create_usd) as base_value,
+    "cost" as field_type,
     1 as monitor_type
 from 
-    {{source("sales","ads_amazon_profit_model_tableau")}}
+    {{source("sales","ads_amazon_profit_model_tableau")}} 
+where 
+    `date` = DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
 group by 
     1 
 
-union 
+union all 
 
 -- 广告
 select 
     `date`,
-    "ads_total_usd" as name,
-    sum(ads_total_usd) as total,
-    1 as monitor_type
+    "ALL" as country,
+    "ads_total_usd" as metric_name,
+    sum(ads_total_usd) as metric_value,
+    sum(net_profit_total_create_usd) as base_value,
+    "cost" as field_type,
+    1 as monitor_type 
 from 
-    {{source("sales","ads_amazon_profit_model_tableau")}}
+    {{source("sales","ads_amazon_profit_model_tableau")}} 
+where 
+    `date` = DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
 group by 
     1 
+
+union all
+
+-------------------------------过去第三十天 利润-------------------------------
+-- 净利润
+select 
+    `date`,
+    "ALL" as country,
+    "net_profit_total_create_usd" as metric_name,
+    sum(net_profit_total_create_usd) as metric_value,
+    sum(net_income_total_create_usd) as base_value,
+    "profit" as field_type,
+    1 as monitor_type 
+from 
+    {{source("sales","ads_amazon_profit_model_tableau")}} 
+where 
+    `date` = DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+group by 
+    1 
+
+union all
+
+-------------------------------过去三十天内 成本-------------------------------
+-- 退款
+select 
+    `date`,
+    "ALL" as country,
+    "refund_b_total_create_usd" as metric_name,
+    sum(refund_b_total_create_usd) as metric_value,
+    sum(net_profit_total_create_usd) as base_value,
+    "cost" as field_type,
+    2 as monitor_type 
+from 
+    {{source("sales","ads_amazon_profit_model_tableau")}} 
+where 
+    `date` > DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+group by 
+    1 
+
+union all
+
+-- 广告
+select 
+    `date`,
+    "ALL" as country,
+    "ads_total_usd" as metric_name,
+    sum(ads_total_usd) as metric_value,
+    sum(net_profit_total_create_usd) as base_value,
+    "cost" as field_type,
+    2 as monitor_type 
+from 
+    {{source("sales","ads_amazon_profit_model_tableau")}} 
+where 
+    `date` > DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+group by 
+    1 
+
+union all 
+
+-------------------------------过去三十天内 利润-------------------------------
+-- 净利润
+select 
+    `date`,
+    "ALL" as country,
+    "net_profit_total_create_usd" as metric_name,
+    sum(net_profit_total_create_usd) as metric_value,
+    sum(net_income_total_create_usd) as base_value,
+    "profit" as field_type,
+    2 as monitor_type 
+from 
+    {{source("sales","ads_amazon_profit_model_tableau")}} 
+where 
+    `date` > DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+group by 
+    1 
+
+union all 
+
+-------------------------------过去三十天内每个站点 利润-------------------------------
+-- 净利润
+select 
+    `date`,
+    country,
+    "net_profit_total_create_usd" as metric_name,
+    sum(net_profit_total_create_usd) as metric_value,
+    sum(net_income_total_create_usd) as base_value,
+    "profit" as field_type,
+    3 as monitor_type  
+from 
+    {{source("sales","ads_amazon_profit_model_tableau")}} 
+where 
+    `date` >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY) 
+group by 
+    1,2
--- tests/monitoring/metric_threshold_check.sql
+++ tests/monitoring/metric_threshold_check.sql
@@ -14,3 +14,77 @@ threshold as (
 )
 
 -- 规则
+WITH base AS (
+  SELECT
+    a.date,
+    a.monitor_type,
+    a.field_type,
+    a.metric_name,
+    a.metric_value,
+    a.base_value,
+    SAFE_DIVIDE(a.metric_value, a.base_value) AS ratio,   -- 公共：实际比率
+    b.min_value,
+    b.max_value
+  FROM metric a
+  JOIN threshold b
+    ON a.metric_name = b.metric_name
+   AND a.monitor_type = b.monitor_type
+),
+
+scored AS (
+  SELECT
+    *,
+    -- 退款状态
+    CASE
+      WHEN monitor_type IN (1, 3) THEN '退款周期未完成'
+      WHEN monitor_type = 2 THEN '退款周期已完成'
+      ELSE NULL
+    END AS refund_status,
+  FROM base
+),
+
+alarm AS (
+  SELECT
+    date,
+    monitor_type,
+    field_type,
+    metric_name,
+    ratio,
+    min_value,
+    max_value,
+    refund_status,
+
+    -- 报警类型（只把“命中的那一种”映射出来）
+    CASE
+      WHEN monitor_type IN (1, 2) AND field_type = 'cost' AND ratio <= min_value THEN '核实'
+      WHEN monitor_type IN (1, 2) AND field_type = 'cost' AND ratio >= max_value THEN '预警'
+
+      WHEN monitor_type IN (1, 2) AND field_type = 'profit' AND ratio <= min_value THEN '预警'
+      WHEN monitor_type IN (1, 2) AND field_type = 'profit' AND ratio >= max_value THEN '核实'
+
+      WHEN monitor_type = 3 AND field_type = 'profit' AND ratio <= min_value THEN '预警'
+      ELSE NULL
+    END AS alarm_type,
+
+    -- 预警值/核实值（跟 alarm_type 同步返回阈值）
+    CASE
+      WHEN monitor_type IN (1, 2) AND field_type IN ('cost','profit') AND ratio <= min_value THEN min_value
+      WHEN monitor_type IN (1, 2) AND field_type IN ('cost','profit') AND ratio >= max_value THEN max_value
+
+      WHEN monitor_type = 3 AND field_type = 'profit' AND ratio <= min_value THEN min_value
+      ELSE NULL
+    END AS alarm_value
+  FROM scored
+)
+
+SELECT
+  date,
+  refund_status,
+  metric_name,
+  alarm_type,
+  alarm_value,
+  ROUND(ratio, 2) AS actual_value,  --实际值
+  ROUND(ratio - alarm_value, 4) AS diff_value,  -- 差值
+  CURRENT_DATE() AS alarm_date
+FROM alarm;
+
